4500dd347bfb9d85e5caf881d04712ef08dcbe8f
lizard699 <elizavetapet@bk.ru>
2022-05-11 15:01:38 +0500

    Создана основная модель кликера

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Core(models.Model):
+    user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
+    coins = models.IntegerField(default=0)
+    click_power = models.IntegerField(default=1)
