cc0dfbf8bf7f8303602c51f1b8c1399b86129da7
ElizavetaPetrova <elizavetapet@bk.ru>
2022-06-06 15:02:12 +0500

    3.6 Расширен функционал кликера

--- backend/urls.py
+++ backend/urls.py
@@ -6,11 +6,18 @@ boosts = views.BoostViewSet.as_view({
     'post': 'create', # Создать буст
 })
 
+
+lonely_boost = views.BoostViewSet.as_view({
+    'put': 'partial_update', # редактировать буст
+})
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.Register.as_view(), name='register'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('call_click/', views.call_click, name='call_click'),
-    path('boosts/', boosts, name='boosts')
+    path('boosts/', boosts, name='boosts'),
+    path('boost/<int:pk>/', lonely_boost, name='boost'),
 ]
